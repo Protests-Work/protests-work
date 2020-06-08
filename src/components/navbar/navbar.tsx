3b2e8ae86ae14f462eb3cc9747dd17518c5b49c1
Emilie Martinez <emiliekarin.martinez@gmail.com>
2020-06-08 10:00:28 -0500

    feat: adding about page, and therefore navigation

---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,31 @@
+/** @jsx jsx */
+import { jsx } from "@emotion/core";
+import { FC, ReactNode } from "react";
+import { Box, Link, Image } from "rebass";
+import Container from "../container/container";
+
+interface Props {
+  className?: string;
+  id?: string;
+  children?: ReactNode;
+}
+
+const Nav: FC<Props & JSX.IntrinsicElements["nav"]> = ({
+  className = "",
+  id = ""
+}) => {
+  return (
+    <Box as="nav" id={id} className={className} py={3}>
+      <Container>
+        <Link href="/" variant="nav" fontSize={5}>
+          Protests Work
+        </Link>
+        <Link href="/about" variant="nav">
+          About
+        </Link>
+      </Container>
+    </Box>
+  );
+};
+
+export default Nav;
