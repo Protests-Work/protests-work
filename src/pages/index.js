44766679e5cd738da3d1073dadef61a0685100ec
Hans Knecht <Hans.knechtions@gmail.com>
2020-06-08 00:21:08 -0400

    chore: removing public feat: adding in new content feat: adding in deployment mechanism documentation: Updating Readme

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,17 +7,18 @@ const Home = () => {
   return (
     <Box
       sx={{
-        maxWidth: 512,
+        maxWidth: 1024,
         mx: 'auto',
         px: 3,
       }}>
-    <h1>This is my site</h1>
+    <h1>Protests Work! Exercise Your Voice and Make Your Will Known!</h1>
+    <p id="subtitle">In the wake of the nearly unprecedented protests on racial inequality, police brutality, and killings of people of color by police, this site was established to document the effects of those protests and the outcomes achieved. This site will attempt to document all aims achieved by protests, but will always represent an incomplete list, feel free to add to it by following <a href="https://github.com/Protests-Work/protests-work">these instructions</a></p>
       {PAGECONTENT.articles.map((item) => (
         <Card bg="white" my={5}>
           <Link href={item.link} variant="primary">
             <h2>{item.title}</h2>
             <Box>
-              <p>{item.date} | {item.state} | {item.protest}</p>
+              <p>{item.date} | {item.location} | {item.protest}</p>
             </Box>
             <p>{item.description}</p>
           </Link>
