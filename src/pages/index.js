e5c22117daae87bec2c9f9d7400ecebe87f7b1a6
Hans Knecht <Hans.knechtions@gmail.com>
2020-06-08 00:54:26 -0400

    adding special thanks to Fahd Ahmed

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,6 +13,7 @@ const Home = () => {
       }}>
     <h1>Protests Work! Exercise Your Voice and Make Your Will Known!</h1>
     <p id="subtitle">In the wake of the nearly unprecedented protests on racial inequality, police brutality, and killings of people of color by police, this site was established to document the effects of those protests and the outcomes achieved. This site will attempt to document all aims achieved by protests, but will always represent an incomplete list, feel free to add to it by following <a href="https://github.com/Protests-Work/protests-work">these instructions</a></p>
+    <p>Special thanks to Fahd Ahmed for compiling the majority of the titles/list of events.</p>
       {PAGECONTENT.articles.map((item) => (
         <Card bg="white" my={5}>
           <Link href={item.link} variant="primary">
